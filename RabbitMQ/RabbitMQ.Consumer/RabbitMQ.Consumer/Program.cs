8b0c013981acfc9b9fe460f1af07bca82f496a6c
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2023-03-10 16:13:17 +0300

    Add files via upload

---
+++ RabbitMQ/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿//bağlantıyı oluşturma (rabbitMQ sunucusuna bağlantı)
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://dofvzijr:Pp-ijBOk2uCxP4za2Fys0bWsVcz8CmCu@vulture.rmq.cloudamqp.com/dofvzijr");
+
+//bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma (messajın okunacağı kuyruğu oluşturma)
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'den mesaj okuma (kuyruktaki mesajları okuma)
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //kuyruğa gelen mesajın işlenmesi
+    //e.Body(): kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray(): kuyruktaki mesajın byte verisine getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://dofvzijr:Pp-ijBOk2uCxP4za2Fys0bWsVcz8CmCu@vulture.rmq.cloudamqp.com/dofvzijr");
+
+//bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// kuyruk oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'ya mesaj gönderme (kuyruğa atacağı mesajları byte türünden kabul eder)
+//byte[] message =  Encoding.UTF8.GetBytes("Test");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Test " + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
